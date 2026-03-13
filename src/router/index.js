928f159e5c6391d28d9897a3696f51c356cb4a7a
Mina-cy <minachenyu08@gmail.com>
2026-03-13 20:48:29 +0800

    fix：解決在其他頁面重整會有404的問題
    1. 因為 Vue 是單頁應用 (SPA)，如果你在 /about 頁面按下瀏覽器的重新整理，GitHub Pages 有可能會顯示 404（因為伺服器找不到 about.html 這個檔案）。在router 中修改了history: createWebHashHistory(import.meta.env.BASE_URL), 改用 Hash 模式可以保證連結永遠有效。

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ import PhotoView from '@/views/PhotoView.vue';
 import AboutView from '@/views/AboutView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
